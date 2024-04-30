14b9684beb4758ee2238374d9c33a8c0a524c912
Twinkle1989 <126779580+Twinkle1989@users.noreply.github.com>
2024-04-30 17:28:53 +0300

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsolanki <tsolanki@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/22 17:51:47 by tsolanki          #+#    #+#             */
+/*   Updated: 2024/04/24 20:20:44 by tsolanki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(char const *str)
+{
+	int			i;
+	int			sign;
+	long int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str [i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+		if (result < 0 && sign == 1)
+			return (-1);
+		else if (result < 0 && sign == -1)
+			return (0);
+	}
+	return ((int)(result * sign));
+}
